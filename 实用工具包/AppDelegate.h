6bb42cf81a6133f6d78c414bf0afc12630af919c
Lishengmin <707159570@qq.com>
2015-12-23 14:18:05 +0800

    Initial Commit

---
+++ 实用工具包/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  实用工具包
+//
+//  Created by Ghoul on 15/12/23.
+//  Copyright © 2015年 Ghoul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 实用工具包/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  实用工具包
+//
+//  Created by Ghoul on 15/12/23.
+//  Copyright © 2015年 Ghoul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
